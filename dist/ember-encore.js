09967993b7687e4f03e3f8a128f21b484e281459
Charles Demers <charles.demers6@gmail.com>
2014-06-03 20:44:43 -0400

    Rebuild with latest changes

--- dist/ember-encore.js
+++ dist/ember-encore.js
@@ -17,7 +17,6 @@
   Ember.onLoad("Ember.Application", function(Application) {
     Application.initializer({
       name: "EmberEncore",
-      after: "ember-data",
       initialize: function(container) {
         container.register("serializer:-encore", EmberEncore.Serializer);
         container.register("adapter:-encore", EmberEncore.Adapter);
